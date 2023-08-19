9277b86e07daceccc028403413d7d871047f5c3b
许相虎 <1163949417@qq.com>
2023-08-19 13:05:58 +0800

    reorder file

--- fix.py
+++
@@ -1,8 +0,0 @@
-with open("decodetree.py", 'r') as infile:
-    with open("fix_decodetree.py", 'w') as outfile:
-            
-        for line in infile.readlines():
-            nline = line.replace(chr(0), '')
-            outfile.write(nline)
-
-
--- parse_a32.sh
+++
@@ -1,2 +0,0 @@
-python3 decodetree.py ./a32-uncond.decode --static-decode=disas_a32_uncond -o ./decode-a32-uncond.c.inc
-python3 decodetree.py ./a32.decode --static-decode=disas_a32 -o ./decode-a32.c.inc
