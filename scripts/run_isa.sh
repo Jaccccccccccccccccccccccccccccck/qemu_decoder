9cc56c00e54e6a0122da73010a438a922dee05c2
root <1163949417@qq.com>
2023-09-15 16:57:19 +0800

    add some

--- scripts/run_isa.sh
+++ scripts/run_isa.sh
@@ -1,2 +1,4 @@
 python3 ./isa/decodetree.py ./isa/a32-uncond.decode --static-decode=disas_a32_uncond -o ./generated/decode-a32-uncond.c.inc
 python3 ./isa/decodetree.py ./isa/a32.decode --static-decode=disas_a32 -o ./generated/decode-a32.c.inc
+
+python3 ./isa_parser/gen_decoder.py ./isa/z86/z86.isa --static-decode=disas_z86 -o ./generated/decode-z86.c.inc
